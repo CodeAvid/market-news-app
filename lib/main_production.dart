3ea640dcb5bf39c50e3541595abd858e47ba3472
Benjamin Akhigbe <51957344+CodeAvid@users.noreply.github.com>
2024-10-05 01:20:12 +0100

    feat(setup-project): add flavor manager to manage multiple configuration environment

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,10 @@
 import 'package:market_news_app/app/app.dart';
+import 'package:market_news_app/app/app_manager.dart';
 import 'package:market_news_app/bootstrap.dart';
+import 'package:market_news_app/flavor_manager.dart';
 
 void main() {
+  FlavorManager.appFlavor = Flavor.production;
+  AppManager.initialize();
   bootstrap(() => const App());
 }
--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,6 +1,10 @@
 import 'package:market_news_app/app/app.dart';
+import 'package:market_news_app/app/app_manager.dart';
 import 'package:market_news_app/bootstrap.dart';
+import 'package:market_news_app/flavor_manager.dart';
 
 void main() {
+  FlavorManager.appFlavor = Flavor.staging;
+  AppManager.initialize();
   bootstrap(() => const App());
 }
