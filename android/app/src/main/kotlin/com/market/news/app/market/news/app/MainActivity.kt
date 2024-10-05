d8eb5719c08556f8cdca1944564e7f9343c0593e
Benjamin Akhigbe <51957344+CodeAvid@users.noreply.github.com>
2024-10-05 01:05:54 +0100

    feat(setup-project): initial project starter

---
+++ android/app/src/main/kotlin/com/market/news/app/market/news/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.market.news.app.market_news_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:market_news_app/app/app.dart';
+import 'package:market_news_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
